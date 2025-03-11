30b82fd1349b1dfa6f77006d3911f9d986329a1e
unknown <EldarFeyzullayev>
2025-03-11 20:31:38 +0400

    hello world

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+class UserID:
+    def __init__(self, fname, age):
+        self.fname = fname
+        self.age = age
+
+fname = input("Enter your first name: ")
+age = int(input("Enter your age: ")) 
+
+status = UserID(fname, age)
+
+print(f"User Details: Name = {status.fname}, Age = {status.age}")
